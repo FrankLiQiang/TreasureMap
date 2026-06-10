2c4f87fbea6c9e0f621a8984d6fa945c3bff074e
李強 <30677343+FrankLiQiang@users.noreply.github.com>
2026-06-10 23:14:03 +0900

    增加说明文字

--- fangyuan/flutter_bootstrap.js
+++ fangyuan/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0a9f488c1306b9d5042a78cffbfef1fa4428b6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "309507302" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2532579180" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
